890d8a42fdb59c66eb24e598021aae9df413dad2
rafliyulistiawan <rafliyulistiawan@gmail.com>
2023-09-17 12:30:57 +0700

    update inout iostream

--- 3_in_out_iostream.cpp
+++ 3_in_out_iostream.cpp
@@ -4,14 +4,19 @@ using namespace std;
 int main()
 {
   char kalimat1[100], kalimat2[100];
+  string kalimat3;
 
   cout << "Masukkan kalimat tanpa spasi: ";
   cin >> kalimat1;
 
-  cout << "Masukkan kalimat: ";
+  cout << "Masukkan kalimat 1: ";
   cin.ignore(); // Untuk mengabaikan enter pada cin baris 9
-  cin.getline(kalimat2, sizeof(kalimat2));
+  cin.getline(kalimat2, 6);
+
+  cout << "Masukkan kalimat 2: ";
+  getline(cin, kalimat3);
 
   cout << "Kalimat = " << kalimat1 << endl;
-  cout << "Kalimat = " << kalimat2;
+  cout << "Kalimat = " << kalimat2 << endl;
+  cout << "Kalimat = " << kalimat3 << endl;
 }
